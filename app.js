aa6f6688cfa809b1e01aac8a78aa8698b35ecf5d
bashar <b.alasmar@ltuc.com>
2019-10-22 15:05:00 +0300

    prob011

--- app.js
+++ app.js
@@ -8,12 +8,19 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum (number1,number2) {
 
-}
+    var resultofsum = number1+number2;
+    var resultofstring='The sum of ' +number1+ ' and '+ number2 + ' is '+ resultofsum+'.'
+    
+        return [resultofsum , resultofstring]
+        
+    }
+//eslint-disable-line
+
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,7 +34,6 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
 }
 
 // Here is the test for multiply(); uncomment it to run it

a4d4384b15a2e5dbddad2f4551e89097aa285778
bashar <b.alasmar@ltuc.com>
2019-10-23 12:48:12 +0300

    prob3

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum (number1,number2) {
+function sum (a,b) {
 
-    var resultofsum = number1+number2;
-    var resultofstring='The sum of ' +number1+ ' and '+ number2 + ' is '+ resultofsum+'.'
+    var resultofsum = a+b;
+    var resultofstring='The sum of ' +a+ ' and '+ b + ' is '+ resultofsum+'.'
     
         return [resultofsum , resultofstring]
         
@@ -57,11 +57,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var sumR = sum(sum(a, b)[0], c);
+    var multiplyR= multiply(multiply(a, b)[0], c);
+    return[sumR[0], multiplyR[0], a + ' and ' + b + ' and ' + c + ' sum to ' + sumR[0] + '.',
+   'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyR[0] + '.'];
 }
-
+    
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
